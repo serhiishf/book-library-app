7ce636b7690ab54f653695c63602ca8d0a0f0036
serhiishf <unison447@gmail.com>
2024-09-12 15:58:07 +0300

    feat: Add reducer for books

---
+++ frontend/src/redux/books/reducer.ts
@@ -0,0 +1,12 @@
+import * as actionTypes from './actionTypes';
+
+const initialState = [];
+
+const booksReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case actionTypes.ADD_BOOK:
+      return [...state, action.payload];
+    default:
+      return state;
+  }
+};
